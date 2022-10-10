d251e96c14bb2f8c97609b223eea80cd429c79aa
Ganipisetti Sai Prasanth <85571247+saiprasanthganipisetti@users.noreply.github.com>
2022-10-10 14:31:29 +0530

    Created `CreateAccount.js` in `/src/components/Accounts`

---
+++ src/components/Accounts/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateAccount () {
+  return (
+    <div className = "createAccount">
+      <h2>Accounts</h2>
+      <h3>Create new account<h3>
+    </div>
+  );
+}
+
+export default CreateAccount;
