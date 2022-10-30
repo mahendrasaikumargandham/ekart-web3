35955d68ce336dc8f73830866d70ed96bb3dac79
199P5A0503 <115029739+199P5A0503@users.noreply.github.com>
2022-10-30 12:12:29 +0530

    Added `CreateInclude` with `Includes.js`

---
+++ src/components/Includes/CreateInclude.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateInclude () {
+  return (
+    <div className="createinclude">
+      <h2>Includes</h2>
+      <h3>Create new Include<h3>
+    </div>
+  );
+}
+
+export default CreateInclude;
